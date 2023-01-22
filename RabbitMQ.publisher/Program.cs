9fa4e10f387aaa6326443c01763e189c2e8f78a1
Bolat Ysf <yusufbolt09@gmail.com>
2023-01-22 22:13:26 +0300

    Add project files.

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+// conncet rabbitmq cloud
+
+factory.Uri = new Uri("amqps://imfunstt:bieg3kNUrb49XGjMrwQ3NUiSt2fYbdsq@shark.rmq.cloudamqp.com/imfunstt");
+
+using var connection=factory.CreateConnection();
+
+// create channel rabbitmq
+
+var channel = connection.CreateModel();
+
+// add queue in channel
+
+channel.QueueDeclare("hello-rabbitmq",true,false,false);
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    // write message and has send
+
+    string message = $"Message{x}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(string.Empty, "hello-rabbitmq", null, messageBody);
+
+    Console.WriteLine($"Message has sended: {message}");
+
+});
+
+
+Console.ReadLine();
---
+++ RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqps://imfunstt:bieg3kNUrb49XGjMrwQ3NUiSt2fYbdsq@shark.rmq.cloudamqp.com/imfunstt");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+//if u create queue in publisher . u would delete queue here!
+
+//channel.QueueDeclare("hello-rabbitmq", true, false, false);
+
+// create consumer
+
+
+channel.BasicQos(0,1,false);
+
+
+var consumer=new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-rabbitmq",false,consumer);
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Thread.Sleep(1500);
+
+    Console.WriteLine("Message:" + message);
+
+    // u can delete sending message queue
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+
+
+
+
+Console.ReadLine();
