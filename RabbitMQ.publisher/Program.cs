880566908f1ac306148cd2f133f16af86d828d4f
Bolat Ysf <yusufbolt09@gmail.com>
2023-01-23 17:11:00 +0300

    DirectExchanges

--- RabbitMQ.publisher/Program.cs
+++ RabbitMQ.publisher/Program.cs
@@ -1,41 +1,59 @@
 ﻿
 
 using RabbitMQ.Client;
+using RabbitMQ.publisher;
 using System.Configuration;
 using System.Text;
 
-var factory = new ConnectionFactory();
 
-// conncet rabbitmq cloud
+
+var factory = new ConnectionFactory();
 
 string conStr = ConfigurationManager.ConnectionStrings["RabbitMQ"].ToString();
 
-
 factory.Uri = new Uri(conStr);
-
-
-
 
 using var connection=factory.CreateConnection();
 
-
 var channel = connection.CreateModel();
 
-// create fanoutexchange
+// create directexchange
 
-//channel.ExchangeDeclare("logs-fanout")
+channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+
+var logList = Enum.GetNames(typeof(LogNames)).ToList();
+
+foreach (var item in logList)
+{
+    var queueName = $"direct-queue-{item}";
+
+    channel.QueueDeclare(queueName,true,false,false);
+
+    var routeKey = $"route-{item}";
+    // bind que
+
+    channel.QueueBind(queueName,"logs-direct",routeKey,null);
+}
+
 
 Enumerable.Range(1, 50).ToList().ForEach(x =>
 {
-    // write message and has send
+    LogNames log =(LogNames) new Random().Next(1, 5);
 
-    string message = $"Message{x}";
+
+    // ll write log type
+
+    string message = $"log-type:{log}";
 
     var messageBody = Encoding.UTF8.GetBytes(message);
 
-    channel.BasicPublish(string.Empty, "hello-rabbitmq", null, messageBody);
+    // add route 
 
-    Console.WriteLine($"Message has sended: {message}");
+    var routeKey=$"route-{log}";
+
+    channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+
+    Console.WriteLine($"Log has sended: {message}");
     
 });
 
--- RabbitMQ.subscriber/Program.cs
+++ RabbitMQ.subscriber/Program.cs
@@ -15,19 +15,14 @@ using var connection = factory.CreateConnection();
 
 var channel = connection.CreateModel();
 
-//if u create queue in publisher . u would delete queue here!
-
-//channel.QueueDeclare("hello-rabbitmq", true, false, false);
-
-// create consumer
-
-
 channel.BasicQos(0,1,false);
-
 
 var consumer=new EventingBasicConsumer(channel);
 
-channel.BasicConsume("hello-rabbitmq",false,consumer);
+// ll connect critical queue
+var queueName = "direct-queue-Info";
+
+channel.BasicConsume(queueName,false,consumer);
 
 consumer.Received += (object sender, BasicDeliverEventArgs e) =>
 {
@@ -35,6 +30,9 @@ consumer.Received += (object sender, BasicDeliverEventArgs e) =>
     Thread.Sleep(1500);
 
     Console.WriteLine("Message:" + message);
+
+    // ll write messages in txt file
+    // File.AppendAllText("log-info.txt", message + "\n");
 
     // u can delete sending message queue
     channel.BasicAck(e.DeliveryTag, false);
